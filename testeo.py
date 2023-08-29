89eadefce235ee44864204fb78a5fdf4a91a198e
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-08-28 21:52:54 -0400

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,16 +1,35 @@
 class libro:
-    def __init__(self,Author,Gender,Date_C,Pages,Publisher):
+    def __init__(self,Author,Gender,Date_C,Pages,Publisher,nombre):
         self.author=Author
         self.gender=Gender
         self.Date_C=Date_C
         self.pages=Pages
         self.publisher=Publisher
-class usuario:
-    def __init__(self,prestamos,ussername,):
-        self.prestamos=prestamos
-        self.usuario=ussername
+        self.nombre=nombre
+    def genero(self):
+        print(f"el genero del libro es {self.gender}")
 class prestamos:
-    def __init__(self,prestamos,fecha_i,fecha_t):
+    def __init__(self,prestamos,fecha_i,fecha_t,libro):
         self.prestamos=prestamos
         self.fechainicio=fecha_i
         self.fechatermino=fecha_t
+        self.libro=libro
+class usuario:
+    def __init__(self,ussername,):
+        self.prestamos= []
+        self.usuario=ussername
+class catalogo:
+    def __init__(self,libro,cantidad,prestamos):
+        self.prestamo = prestamos
+        self.cantidad=cantidad
+        self.libro=libro
+
+libro1=libro("tadeus manus","terror","14/03/2022","234","lapilopes","las aventuras de paco")
+libro2=libro("paco cabezas","fantasia","28/08/2023","40","tartarus company","alimac_leblanc")
+libro3=libro("luan vazques","misterio","02/12/2021","345","pandoria")
+libro4=libro("natalia cáceres","policias","31/11/2004","222","bnjiboi")
+
+usuario1=usuario("luci",)
+usuario2=usuario("","")
+usuario3=usuario("","")
+
