7e84d1d9f08ba6a057762c3a06afc8fd87f800d9
Cristian0Caceres <134634536+Cristian0Caceres@users.noreply.github.com>
2023-08-30 00:31:09 -0400

    Update testeo.py

--- testeo.py
+++ testeo.py
@@ -1,29 +1,30 @@
+LIST_USERS = []
+
 class Book:                                                #Clase Libro
-    def __init__(self,name,author,pages,gender):#creamos la clase book con los parametros de name author pages and gender 
+    def __init__(self,id,name,author,pages,gender):
         self.id = id
         self.name = name
         self.author = author
         self.pages = pages
         self.gender = gender
-        self.state = "Disponible" #establecemos el estado inicial como disponible en cada libro 
+        self.state = "Disponible"
 
     
 class Catalog:        
-                                                  #Clase Catologo
-    def __init__(self):#creamos la clase de catalogo la cual tendra en su interior una lista como atrivuto 
+    def __init__(self):
         self.catalogList = []
 
-    def AddBook(self,book):#creamos la siguiente funcion la cual añade libros a la lista dentro de catalogo
+    def AddBook(self,book):
         self.catalogList.append(book)
 
-    def DeleteBook(self,name):#la siguiente funcion se utilisara parra eliminar los libros dentro del catalogo
+    def DeleteBook(self,name):
         index = 0
         for book in self.catalogList:
             if book.name == name:
                 self.catalogList.pop(index)
                 break
 
-    def ShowCatalog(self):#la siguiente funcion se usa para mostrar los elementos en el interior del catalog 
+    def ShowCatalog(self):
         for book in self.catalogList:
             if book.state == "Disponible":
                 print("Titulo: {0} Autor: {1} Genero: {2} Disponibilidad: {3}".format(book.name,book.author,book.gender,book.state))
@@ -31,32 +32,33 @@ class Catalog:
 
 
 class User:                                             #Clase Usuario
-    def __init__(self,id,name,lastname,age):#creamos la clase de usuarios la cual tiene id , name , lastname , age
+    def __init__(self,id,name,lastname,age):
         self.id = id
         self.name = name
         self.lastname = lastname
         self.age = age
         self.userborrowedBooks= []
     
-    def ShowBorrowedBooks(self):#la funcionalidad de esta funcion recae en el mostrar los libros prestados 
+    def ShowBorrowedBooks(self):
         for books in self.userborrowedBooks:
             print(books)
 
-    def ShowInformation(self):#la siguiente funcion se centra en mostrar  la informacion del usuario
+    def ShowInformation(self):
         print("Id: ", self.id)
         print("Nombre: ", self.name, self.lastname)
         print("Edad: ", self.age)
         print("Libros Prestados ", self.userborrowedBooks)
         
-class lend:                                          #Clase prestamo
-    def __init__(self,book,user,dateInit,dateExit):#creamos la clase lend con los siguientes datos book usser date init date exit y una lista para almacenar los libros prestados
+class load:                                          #Clase prestamo
+    def __init__(self,book,user,dateInit,dateExit):
         self.book = book
         self.user = user
         self.dateInit = dateInit
         self.dateExit = dateExit
         self.borrowedBooks = []
+        
 
-    def LendBook(self,catalog,user):#la siguiente funcion consiste en agregar los libros prestados a la lista de libros prestados dentro de la clase lend
+    def LendBook(self,catalog,user):
         
         for book in catalog.catalogList:
             if book.name == self.book and book.state == "Disponible":
@@ -65,14 +67,30 @@ class lend:                                          #Clase prestamo
 
                 
 
-    def ShowLendBook(self):#la siguiente  funcion tiene la principal funcion de mostrar los libros
+    def ShowLendBook(self):
         for i in self.borrowedBooks:
             print(i)
+
+
+def Clean():
+    print("\n")
+
+
+
+def ShowUser():
+    Clean()
+    for i in LIST_USERS:
+        print(i.id,i.name,i.lastname)
+
+def GetId():
+    return len(LIST_USERS)
+    
 
 def Welcome():
     print("Bienvenido")
 
 def Options():
+    Clean()
     print("1. Registrar Usuario")
     print("2. Crear Libro")
     print("3. Eliminar Libro")
@@ -80,6 +98,7 @@ def Options():
     print("5. Devolver Libro")
     print("6. Ver libros disponibles")
     print("7. Ver historial de un usuario")
+    print("8. Mostrar todos los usuarios")
     print("0. Salir")
     
 def main():
@@ -90,15 +109,17 @@ def main():
         Options()
         userOptions = int(input("> "))
         if userOptions == 1:
-            userId = input("Id: ")
+            userId = GetId()
             userName = input("Nombre: ")
             aux = userName
             userLastname = input("Apellido: ")
             userAge = int(input("Edad: "))
-            
+    
             globals()[aux] = User(userId,userName,userLastname,userAge)
             
-            globals()[aux].ShowInformation()
+            LIST_USERS.append(globals()[aux])
+            
+            ShowUser()
             
         elif userOptions == 2:
             bookName = input("Nombre del libro: ")
@@ -107,10 +128,18 @@ def main():
             bookPages = input("Paginas: ")
             bookGender = input("Genero: ")
             globals()[aux] = Book(bookName,bookAuthor,bookPages,bookGender)
-        
+            CATALOG.catalogList.append(globals()[aux])
+            
         elif userOptions == 3:
             bookName = input("Nombre del libro: ")
-            CATALOG.DeleteBook
+            CATALOG.DeleteBook(bookName)
+            
+            
+            
+        
+        elif userOptions == 6:
+            CATALOG.ShowCatalog()
+            
             
             
             
@@ -122,6 +151,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
